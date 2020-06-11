e630c63ee31c3fb65220c29a84baec08ae93f1fb
sitharak04 <sithara.magnetron@gmail.com>
2020-06-11 00:00:56 -0400

    Add new files

---
+++ Numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+a=np.array([1,2,3,4])
+print(a/2)
+b=np.array([1,'a'])
+print(b)
+c=[1,2,3,4]
+print(c)
+d=[1,'a',4]
+print(d)
+#e=np.array([1,2+i,3], "complex")
+e= np.arange(20,dtype='complex')
+print(e)
+f=np.ones((3,3))
+print(f)
---
+++ python.py
@@ -0,0 +1,15 @@
+print("hello")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+print(a)
+
+# plt.scatter(a,b,alpha=0.3)
+# plt.show()
+# import mysql.connector as ms
+
+for i in range(1,10):
+    print(i)
+
